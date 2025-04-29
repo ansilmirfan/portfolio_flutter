e19c683fa2b56d0e31a875c3b06a5655cadb29d5
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-30 01:05:58 +0530

    feat : added extension on BuildContext for easier navigation purposes

--- lib/core/utils/extension/extensions.dart
+++ lib/core/utils/extension/extensions.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+
 extension Propotion on double {
   double multiply(double factor) => this * factor;
+}
+
+//extension on BuildContext for naviagation Purposes
+extension Navigation on BuildContext {
+  void pop() => Navigator.of(this).pop();
+  void push(Widget page) =>
+      Navigator.of(this).push(MaterialPageRoute(builder: (context) => page));
 }
