b0c539f4e3d822a73d5288979baa422c9b38c65e
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-29 22:11:57 +0530

    feat: added themedata to the MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/features/presentation/screens/home.dart';
+import 'package:portfolio/features/presentation/themes/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.themeData,
+      home: Home(),
+    );
   }
 }
