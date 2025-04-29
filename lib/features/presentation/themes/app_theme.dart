f6a9b24d50efaf27bd011bd5adbe5728788ed8e7
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-29 21:57:03 +0530

    feat : added Text theme and scaffold background color into themedata

--- lib/features/presentation/themes/app_theme.dart
+++ lib/features/presentation/themes/app_theme.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/features/presentation/themes/app_text_theme.dart';
 import 'package:portfolio/features/presentation/themes/color/app_colors.dart';
 
 class AppTheme {
-  static ThemeData themeData = ThemeData(colorScheme: colorScheme);
+  static ThemeData themeData = ThemeData(
+    colorScheme: colorScheme,
+    textTheme: textTheme,
+    scaffoldBackgroundColor: AppColors.bgColor,
+  );
   //color scheme
   static final colorScheme = ColorScheme(
     brightness: Brightness.dark,
@@ -15,5 +20,14 @@ class AppTheme {
     onSurface: AppColors.white,
     error: Colors.red,
     onError: AppColors.white,
+  );
+  //text theme
+  static TextTheme textTheme = TextTheme(
+    bodyMedium: AppTextTheme.body,
+    bodyLarge: AppTextTheme.bodyBoldWhite,
+    bodySmall: AppTextTheme.bodyBoldPrimary,
+    headlineLarge: AppTextTheme.boldTitle,
+    headlineMedium: AppTextTheme.boldTitlePrimary,
+    headlineSmall: AppTextTheme.thinTitle,
   );
 }
