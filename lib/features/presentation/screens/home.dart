03a592a7c54de8f07fad3b9c997b20fd7ba95d42
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-30 00:16:25 +0530

    fix :updated home page SliverAppBar to CustomAppBar widget that returns a SliverAppBar

--- lib/features/presentation/screens/home.dart
+++ lib/features/presentation/screens/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/features/presentation/widgets/custom_appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: CustomScrollView(slivers: [SliverAppBar()]));
+    return Scaffold(body: CustomScrollView(slivers: [CustomAppbar()]));
   }
 }
