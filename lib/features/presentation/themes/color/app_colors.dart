b756ae33f33366b12cec9dd54d44d6c7ba59ed78
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-29 20:47:31 +0530

    feat : added AppColors class

---
+++ lib/features/presentation/themes/color/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primary = Color(0xFFC778DD);
+  static final Color bgColor = Color(0xFF282C33);
+  static final Color grey = Color(0xFFABB2BF);
+  static final Color white = Color(0xFFFFFFFF);
+}
