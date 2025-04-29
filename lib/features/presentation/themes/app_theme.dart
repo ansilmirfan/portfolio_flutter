5c3d9c9c38a191394ec0bde6e5c555418eebf358
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-29 21:16:04 +0530

    feat: added colorScheme in Themedata

--- lib/features/presentation/themes/app_theme.dart
+++ lib/features/presentation/themes/app_theme.dart
@@ -1,1 +1,19 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:portfolio/features/presentation/themes/color/app_colors.dart';
+
+class AppTheme {
+  static ThemeData themeData = ThemeData(colorScheme: colorScheme);
+  //color scheme
+  static final colorScheme = ColorScheme(
+    brightness: Brightness.dark,
+    primary: AppColors.primary,
+    onPrimary: AppColors.white,
+    secondary: AppColors.grey,
+    onSecondary: AppColors.white,
+
+    surface: AppColors.bgColor,
+    onSurface: AppColors.white,
+    error: Colors.red,
+    onError: AppColors.white,
+  );
+}
