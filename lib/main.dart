e3cd37e7e51a6712e7b273695393013506c76d80
ansilmirfan <ansilmirfan123@gmail.com>
2025-04-29 22:04:50 +0530

    feat(ui) : added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/features/presentation/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
