6c4b32804844da60bd93f088587459a7884171a0
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-04 20:01:30 +0530

    refactor : added pushTransperent method in BuildContext extention    -contains a slide animation from bottom to top    -transperent background

--- lib/core/utils/extension/extensions.dart
+++ lib/core/utils/extension/extensions.dart
@@ -9,4 +9,25 @@ extension Navigation on BuildContext {
   void pop() => Navigator.of(this).pop();
   void push(Widget page) =>
       Navigator.of(this).push(MaterialPageRoute(builder: (context) => page));
+  //navigation with transoerent bakcground
+  void pushTransperent(Widget page) => Navigator.of(this).push(
+    PageRouteBuilder(
+      opaque: false,
+      barrierDismissible: true,
+      transitionDuration: Duration(milliseconds: 400),
+      reverseTransitionDuration: Duration(milliseconds: 400),
+      barrierColor: Colors.black45,
+
+      pageBuilder: (context, animation, secondaryAnimation) {
+        final slideTransition = Tween(
+          begin: Offset(0, 1),
+          end: Offset.zero,
+        ).animate(animation);
+        return Opacity(
+          opacity: animation.value,
+          child: SlideTransition(position: slideTransition, child: page),
+        );
+      },
+    ),
+  );
 }
