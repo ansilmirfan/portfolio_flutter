2b517d515694f78c0eb27e8f29c0f6063e426eab
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-04 19:09:24 +0530

    refactor : added hovering effect   -added linear gradient border only while hovering   -added rotation animation for border

--- lib/features/presentation/widgets/bordered_text_button.dart
+++ lib/features/presentation/widgets/bordered_text_button.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class BorderedTextButton extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:portfolio/features/presentation/themes/color/app_colors.dart';
+
+class BorderedTextButton extends StatefulWidget {
   final VoidCallback onTap;
   final String text;
   final bool dark;
@@ -28,37 +31,121 @@ class BorderedTextButton extends StatelessWidget {
   }
 
   @override
+  State<BorderedTextButton> createState() => _BorderedTextButtonState();
+}
+
+class _BorderedTextButtonState extends State<BorderedTextButton>
+    with SingleTickerProviderStateMixin {
+  bool isHover = false;
+  late AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     //fi the mini is true then a lesser font size
     double? fontSize;
-    if (mini) {
+    if (widget.mini) {
       fontSize = 10;
     }
-    return TextButton(
-      style: ButtonStyle(
-        shape: WidgetStatePropertyAll(
-          //if the dark is true then the border color will be grey otherwise it will be primary color
-          BeveledRectangleBorder(
-            side: BorderSide(
-              color:
-                  dark
-                      ? Theme.of(context).colorScheme.secondary
-                      : Theme.of(context).colorScheme.primary,
+    return AnimatedBuilder(
+      animation: controller,
+      builder:
+          (context, child) => CustomPaint(
+            //the linear border is only visible in hvering state
+            painter: isHover ? LinearBorder(animation: controller) : null,
+            child: TextButton(
+              onHover: (value) => isHovering(value),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  Theme.of(context).colorScheme.surface,
+                ),
+                shape: WidgetStatePropertyAll(
+                  //if the dark is true then the border color will be grey otherwise it will be primary color
+                  BeveledRectangleBorder(
+                    side:
+                        isHover
+                            ? BorderSide(color: Colors.transparent)
+                            : BorderSide(
+                              color:
+                                  widget.dark
+                                      ? Theme.of(context).colorScheme.secondary
+                                      : Theme.of(context).colorScheme.primary,
+                            ),
+                  ),
+                ),
+              ),
+              onPressed: widget.onTap,
+              //if the dark is true then the text will be in grey with lesser weight otherwise white color whith more fontweight
+              child: Text(
+                widget.text,
+                style:
+                    widget.dark
+                        ? Theme.of(context).textTheme.bodyMedium
+                        : Theme.of(
+                          context,
+                        ).textTheme.bodyLarge?.copyWith(fontSize: fontSize),
+              ),
             ),
           ),
-        ),
-      ),
-      onPressed: onTap,
-      //if the dark is true then the text will be in grey with lesser weight otherwise white color whith more fontweight
-      child: Text(
-        text,
-        style:
-            dark
-                ? Theme.of(context).textTheme.bodyMedium
-                : Theme.of(
-                  context,
-                ).textTheme.bodyLarge?.copyWith(fontSize: fontSize),
-      ),
     );
+  }
+
+  isHovering(bool value) {
+    setState(() {
+      isHover = value;
+    });
+    if (isHover) {
+      controller
+        ..forward()
+        ..repeat();
+    } else {
+      controller.stop();
+    }
+  }
+}
+
+//custom class for linear gradient border with aniamtion
+class LinearBorder extends CustomPainter {
+  AnimationController animation;
+  LinearBorder({required this.animation}) : super(repaint: animation);
+  @override
+  void paint(Canvas canvas, Size size) {
+    //rectangle for drawing the border
+    Rect rect = Rect.fromPoints(Offset.zero, Offset(size.width, size.height));
+    Gradient gradient = LinearGradient(
+      colors: [
+        AppColors.primary,
+        AppColors.primary,
+        Colors.transparent,
+        Colors.transparent,
+      ],
+      //gradient rotation
+      transform: GradientRotation(animation.value * (2 * pi)),
+    );
+    Paint paint =
+        Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 4
+          ..shader = gradient.createShader(rect);
+
+    canvas.drawRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
