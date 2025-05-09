871468441cee90b8419f4f6ca841b702a88b0847
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-09 21:39:12 +0530

    refactor : updated image with new path and color

--- lib/features/presentation/screens/sections/widgets/profile_image_panel.dart
+++ lib/features/presentation/screens/sections/widgets/profile_image_panel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/core/utils/extension/extensions.dart';
 import 'package:portfolio/core/utils/screen_utils.dart';
+import 'package:portfolio/features/presentation/themes/color/app_colors.dart';
 import 'package:portfolio/features/presentation/widgets/bordered_container.dart';
 import 'package:portfolio/features/presentation/widgets/dot_container.dart';
 
@@ -14,7 +15,7 @@ class ProfileImagePanel extends StatelessWidget {
       children: [
         Container(
           constraints: BoxConstraints(
-            maxHeight: ScreenUtils.height(context).multiply(0.6),
+            maxHeight: ScreenUtils.height(context).multiply(0.5),
           ),
           width: width,
 
@@ -25,10 +26,14 @@ class ProfileImagePanel extends StatelessWidget {
               //logo in the background
               Align(
                 alignment: Alignment(-1.0, -1.0),
-                child: Image.network('assets/images/logo.png', width: 250),
+                child: Image.network('assets/images/logo.png', width: 200),
               ),
               //user image
-              Image.network('assets/images/hoodie1.png'),
+              Image.network(
+                'assets/images/am.png',
+                color: AppColors.primary,
+                width: 250,
+              ),
               Positioned(
                 bottom: 0,
                 left: width.multiply(0.75),
