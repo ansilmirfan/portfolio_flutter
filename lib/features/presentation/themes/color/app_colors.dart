00ec0c0ced537e4a3a3b19330abd4559fa4bfa7b
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-10 21:37:07 +0530

    refactor : updated AppColors with new colors

--- lib/features/presentation/themes/color/app_colors.dart
+++ lib/features/presentation/themes/color/app_colors.dart
@@ -5,4 +5,7 @@ class AppColors {
   static final Color bgColor = Color(0xFF282C33);
   static final Color grey = Color(0xFFABB2BF);
   static final Color white = Color(0xFFFFFFFF);
+  static final Color google = Color(0xFF0B57CF);
+  static final Color linkedin = Color(0xFF0B66C3);
+  static final Color whatsApp = Color(0xFF25D467);
 }
