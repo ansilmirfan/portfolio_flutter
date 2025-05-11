5496c19e437850086e7a259726761ffd7110ffda
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-11 10:45:00 +0530

    refactor : updated widgets with slide animation

--- lib/features/presentation/screens/sections/footer_section.dart
+++ lib/features/presentation/screens/sections/footer_section.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:portfolio/core/constants/url.dart';
+import 'package:portfolio/core/constants/animated_icons.dart';
 import 'package:portfolio/core/utils/extension/extensions.dart';
 import 'package:portfolio/core/utils/screen_utils.dart';
-import 'package:portfolio/features/data/services/url_launcher_service.dart';
-import 'package:portfolio/features/presentation/themes/color/app_colors.dart';
-import 'package:portfolio/features/presentation/widgets/animated_icon.dart';
 import 'package:portfolio/features/presentation/widgets/gap.dart';
 
 class FooterSection extends StatelessWidget {
@@ -47,8 +43,8 @@ class FooterSection extends StatelessWidget {
                         style: Theme.of(context).textTheme.bodyLarge,
                       ),
                     ],
-                  ),
-                  Text("Flutter Developer"),
+                  ).slideToRight(delay: 200),
+                  Text("Flutter Developer").slideToRight(delay: 250),
                 ],
               ),
             ),
@@ -60,26 +56,10 @@ class FooterSection extends StatelessWidget {
                 mainAxisAlignment:
                     isMobile ? MainAxisAlignment.center : MainAxisAlignment.end,
                 children: [
-                  AnimationIcon(
-                    icon: FontAwesomeIcons.solidEnvelope,
-                    ontap: () => UrlLauncherService.launch(Url.email),
-                    color: AppColors.google,
-                  ),
-                  AnimationIcon(
-                    icon: FontAwesomeIcons.whatsapp,
-                    ontap: () => UrlLauncherService.launch(Url.whatsapp),
-                    color: AppColors.whatsApp,
-                  ),
-                  AnimationIcon(
-                    icon: FontAwesomeIcons.linkedin,
-                    ontap: () => UrlLauncherService.launch(Url.linkedin),
-                    color: AppColors.linkedin,
-                  ),
-                  AnimationIcon(
-                    icon: FontAwesomeIcons.github,
-                    ontap: () => UrlLauncherService.launch(Url.gitHub),
-                    color: AppColors.grey,
-                  ),
+                  AnimIcon.email.slideToLeft(delay: 300),
+                  AnimIcon.whatsApp.slideToLeft(delay: 350),
+                  AnimIcon.linkedin.slideToLeft(delay: 400),
+                  AnimIcon.gitHub.slideToLeft(delay: 450),
                 ],
               ),
             ),
